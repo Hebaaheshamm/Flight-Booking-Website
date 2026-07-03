ffb40f2ad8ca9a25aad5d41b796e4dbadefe4007
Hebaaheshamm <Heba.Mohamed.FCI2024058@sadatacademy.edu.eg>
2026-07-03 20:43:21 +0300

    full website is done

---
+++ script.js
@@ -0,0 +1,21 @@
+function bookFlight() {
+
+    alert("Flight booked successfully.");
+
+    window.location = "trips.html";
+
+}
+
+function continueBooking() {
+
+    alert("Passenger information saved.");
+
+    window.location = "notifications.html";
+
+}
+
+function downloadReceipt() {
+
+    alert("Receipt downloaded successfully.");
+
+}
